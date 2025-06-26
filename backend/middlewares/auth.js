51e192e17242fb1d162dd82ad48527cf12a7f659
imtiaza1 <emtiazaleee@gmail.com>
2025-06-26 16:08:43 +0500

    req.cookies.token use kya hei

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,19 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  const token = req.cookies.token;
 
-  if (authHeader && authHeader.startsWith("Bearer ")) {
-    const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "No token provided!" });
+  }
 
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-      next();
-    } catch (error) {
-      return res.status(401).json({ message: "Invalid or expired token" });
-    }
-  } else {
-    return res.status(401).json({ message: "No token provided" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // decoded should contain user id, role, etc.
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
