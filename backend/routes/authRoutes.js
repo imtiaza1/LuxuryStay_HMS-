d09cbd510d65a6478575f6e3bd75337e008170b1
imtiaza1 <emtiazaleee@gmail.com>
2025-06-27 18:11:04 +0500

    fix: update auth routes to add login and register validation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { loginUser } from "../controllers/authController.js";
-import { validateUser } from "../middlewares/validateUser.js";
 
 const router = express.Router();
 
 // POST /api/auth/login
-router.post("/login", validateUser, loginUser);
+router.post("/login", loginUser);
 
 export default router;
