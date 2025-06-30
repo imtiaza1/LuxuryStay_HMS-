92cfb17be183bce8cf2866b7e7bf06f44d95dc0c
imtiaza1 <emtiazaleee@gmail.com>
2025-06-30 15:31:57 +0500

    update routes

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -11,26 +11,35 @@ import { restrictTo } from "../middlewares/role.js";
 
 const router = express.Router();
 
-//
+// create bookings
 router.post(
-  "/",
+  "/create",
   protect,
   restrictTo("admin", "manager", "receptionist", "guest"),
   createBooking
 );
+//get all bookings
 router.get(
   "/",
   protect,
   restrictTo("admin", "manager", "receptionist"),
   getAllBookings
 );
-router.get("/my-bookings", protect, restrictTo("guest"), getMyBookings);
+//get guestbookings
+router.get(
+  "/my-bookings",
+  protect,
+  restrictTo("guest", "admin"),
+  getMyBookings
+);
+//update bookings
 router.put(
   "/:id",
   protect,
   restrictTo("admin", "manager", "receptionist"),
   updateBookingStatus
 );
+//delete bookings
 router.delete("/:id", protect, restrictTo("admin", "manager"), deleteBooking);
 
 export default router;
