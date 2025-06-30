0bc9d0e570b4b4713027a80656dafbef052682df
imtiaza1 <emtiazaleee@gmail.com>
2025-06-30 15:35:47 +0500

    add routes

---
+++ backend/routes/billingroutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  confirmOnlineBilling,
+  createCashBilling,
+  createPaymentIntent,
+  deleteBilling,
+  getAllBillings,
+  getBilling,
+  updateBilling,
+} from "../controllers/billingController.js";
+import { protect } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post(
+  "/cash",
+  protect("admin", "receptionist", "manager"),
+  createCashBilling
+);
+router.post("/payment-intent", createPaymentIntent); // create stripe payment intent
+router.post("/confirm", confirmOnlineBilling); // confirm after success
+
+router.get("/", getAllBillings);
+router.get("/:id", getBilling);
+router.put("/:id", updateBilling);
+router.delete("/:id", deleteBilling);
+
+export default router;
