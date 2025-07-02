126b2d660901cc670f6fda237dcac6c1a9e855cf
imtiaza1 <emtiazaleee@gmail.com>
2025-07-02 18:33:11 +0500

    add review & task features

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  createTask,
+  deleteTask,
+  getTaskById,
+  getTasks,
+  updateTask,
+} from "../controllers/taskController.js";
+import { protect } from "../middlewares/auth.js";
+import { restrictTo } from "../middlewares/role.js";
+
+const router = express.Router();
+
+// Admins or managers can create tasks
+router.post("/create", protect, restrictTo("admin", "manager"), createTask);
+
+// All staff can view tasks
+router.get("/", protect, getTasks);
+router.get("/:id", protect, getTaskById);
+
+// Update or delete by admin or manager only
+router.put("/update/:id", protect, restrictTo("admin", "manager"), updateTask);
+router.delete("/:id", protect, restrictTo("admin", "manager"), deleteTask);
+
+export default router;
